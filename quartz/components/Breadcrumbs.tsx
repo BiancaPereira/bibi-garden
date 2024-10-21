238f000e3e48324d0e7442586fadf4e5010eaa23
Bianca Pereira <heybiibis@gmail.com>
2024-10-21 16:33:28 -0300

    atualiza breadcrumb

--- quartz/components/Breadcrumbs.tsx
+++ quartz/components/Breadcrumbs.tsx
@@ -34,7 +34,7 @@ interface BreadcrumbOptions {
 
 const defaultOptions: BreadcrumbOptions = {
   spacerSymbol: "❯",
-  rootName: "Home",
+  rootName: "Início",
   resolveFrontmatterTitle: true,
   hideOnRoot: true,
   showCurrentPage: true,
